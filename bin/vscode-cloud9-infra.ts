c44134e2db8cc6a77064606cf4e14cc71858db12
sourabh pisal <pisal.sourabh@gmail.com>
2024-01-06 16:03:06 +0000

    Enable liniting

--- bin/vscode-cloud9-infra.ts
+++ bin/vscode-cloud9-infra.ts
@@ -1,21 +1,8 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { VscodeCloud9InfraStack } from '../lib/vscode-cloud9-infra-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { VscodeCloud9InfraStack } from '../lib/vscode-cloud9-infra-stack'
 
-const app = new cdk.App();
-new VscodeCloud9InfraStack(app, 'VscodeCloud9InfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const app = new cdk.App()
+const infra = new VscodeCloud9InfraStack(app, 'VscodeCloud9InfraStack', {})
+cdk.Tags.of(infra).add('Project', 'vscode-cloud9')
