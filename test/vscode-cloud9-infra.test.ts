7800b9811e4ecee2f53fc7a4de6dd2995a05325d
sourabh pisal <pisal.sourabh@gmail.com>
2024-01-06 15:41:18 +0000

    Cdk init

---
+++ test/vscode-cloud9-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VscodeCloud9Infra from '../lib/vscode-cloud9-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vscode-cloud9-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VscodeCloud9Infra.VscodeCloud9InfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
